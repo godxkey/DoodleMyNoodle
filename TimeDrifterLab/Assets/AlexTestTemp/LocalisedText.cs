57c9b7174fadaa47c842f148ffd34b3966af6ceb
Xeal <alexandre-blanchet94@hotmail.com>
2018-11-20 23:06:39 -0500

    Push of the first ideas for the localisation system

---
+++ TimeDrifterLab/Assets/AlexTestTemp/LocalisedText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalisedText
+{
+    string editorText;
+    string applicationText;
+
+    LocalisedText(string text)
+    {
+        editorText = text;
+    }
+
+    public static implicit operator LocalisedText(string input)
+    {
+        LocalisedText output = new LocalisedText(input);
+        return output;
+    }
+
+    public static implicit operator string(LocalisedText input)
+    {
+#if UNITY_EDITOR
+        return input.editorText;
+#endif
+        return input.applicationText;
+    }
+}
