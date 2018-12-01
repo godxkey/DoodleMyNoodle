c30ae1b0a8927919277c27a5120fcdc9b553e7aa
Xeal <alexandre-blanchet94@hotmail.com>
2018-12-01 17:15:41 -0500

    Added 2 tools, one for directories (you can add logo and colors) and another one for Task manager.

---
+++ TimeDrifterLab/Assets/Plugins/NestedTODO/Scripts/Editor/UserTweaks.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace NestedTODO
+{
+    public class UserTweaks
+    {
+        //deprecated class
+        //keept only to prevents erros when updating versions
+    }
+}
