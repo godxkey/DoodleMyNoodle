1053f39f28914f72516f0dce9594a4d866b8ed47
Frédéric Bessette <msfredb7@gmail.com>
2018-12-02 22:54:52 -0500

    Port animations

---
+++ TimeDrifterLab/Assets/Plugins/CCC/Code/Core/Animation/AlphaAnimationFlash.cs
@@ -0,0 +1,187 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AlphaAnimationFlash : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool timeScaleIndependant = false;
+    public ComponentType componentType = ComponentType.Nothing;
+
+    [Header("Behaviour")]
+    public EnableBehaviour onEnable = EnableBehaviour.Restart;
+    public DisableBehaviour onDisable = DisableBehaviour.Stop;
+
+    [Header("Animation")]
+    public bool transparentAtStart = false;
+    public float fadeDuration = 1f;
+    public Ease ease = Ease.Linear;
+
+    public enum ComponentType { Nothing = 0, Text = 1, Image = 2, Sprite = 3, CanvasGroup = 4 }
+    public enum EnableBehaviour { Nothing, Resume, Restart }
+    public enum DisableBehaviour { Nothing, Pause, Stop }
+
+    private Tween tween;
+    public float OriginalAlpha { get; set; }
+
+    void Awake()
+    {
+        OriginalAlpha = GetCurrentAlpha();
+    }
+
+    void OnEnable()
+    {
+        switch (onEnable)
+        {
+            case EnableBehaviour.Restart:
+                Restart();
+                break;
+            case EnableBehaviour.Resume:
+                Play();
+                break;
+        }
+    }
+
+    void OnDisable()
+    {
+        switch (onDisable)
+        {
+            case DisableBehaviour.Pause:
+                Pause();
+                break;
+            case DisableBehaviour.Stop:
+                Stop();
+                break;
+        }
+    }
+
+    public void Stop()
+    {
+        if (tween != null && tween.IsActive())
+            tween.Kill();
+    }
+
+    public void Play()
+    {
+        if (tween != null && tween.IsActive())
+            tween.Play();
+        else
+            StartAnimation();
+    }
+
+    public void Pause()
+    {
+        if (tween != null && tween.IsPlaying())
+            tween.Pause();
+    }
+
+    public void Restart()
+    {
+        if (tween != null && tween.IsPlaying())
+            tween.Restart();
+        else
+            StartAnimation();
+    }
+
+    void StartAnimation()
+    {
+        if (componentType == ComponentType.Nothing)
+            return;
+
+        Stop();
+
+        float begin = transparentAtStart ? 0 : OriginalAlpha;
+        float end = transparentAtStart ? OriginalAlpha : 0;
+
+        switch (componentType)
+        {
+            case ComponentType.Text:
+                {
+                    Text text = GetComponent<Text>();
+                    if (text == null)
+                        return;
+
+                    text.color = text.color.ChangedAlpha(begin);
+                    tween = text.DOFade(end, fadeDuration);
+
+                    break;
+                }
+            case ComponentType.Image:
+                {
+                    Image image = GetComponent<Image>();
+                    if (image == null)
+                        return;
+
+                    image.SetAlpha(begin);
+                    tween = image.DOFade(end, fadeDuration);
+
+                    break;
+                }
+            case ComponentType.Sprite:
+                {
+                    SpriteRenderer sprRenderer = GetComponent<SpriteRenderer>();
+                    if (sprRenderer == null)
+                        return;
+
+                    sprRenderer.SetAlpha(begin);
+                    tween = sprRenderer.DOFade(end, fadeDuration);
+
+                    break;
+                }
+            case ComponentType.CanvasGroup:
+                {
+                    CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+                    if (canvasGroup == null)
+                        return;
+
+                    canvasGroup.alpha = begin;
+                    tween = canvasGroup.DOFade(end, fadeDuration);
+
+                    break;
+                }
+            default:
+                break;
+        }
+
+        tween?.SetEase(ease)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    float GetCurrentAlpha()
+    {
+        switch (componentType)
+        {
+            case ComponentType.Text:
+                {
+                    Text text = GetComponent<Text>();
+                    if (text != null)
+                        return text.color.a;
+                    break;
+                }
+            case ComponentType.Image:
+                {
+                    Image image = GetComponent<Image>();
+                    if (image != null)
+                        return image.color.a;
+                    break;
+                }
+            case ComponentType.Sprite:
+                {
+                    SpriteRenderer sprRenderer = GetComponent<SpriteRenderer>();
+                    if (sprRenderer != null)
+                        return sprRenderer.color.a;
+                    break;
+                }
+            case ComponentType.CanvasGroup:
+                {
+                    CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+                    if (canvasGroup != null)
+                        return canvasGroup.alpha;
+                    break;
+                }
+        }
+        Debug.LogError("Wrong 'ComponentType' on " + gameObject.name + "'s " + nameof(AlphaAnimationFlash));
+        return 1;
+    }
+}
---
+++ TimeDrifterLab/Assets/Plugins/CCC/Code/Core/Animation/TransformAnimationFloating.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class TransformAnimationFloating : MonoBehaviour
+{
+    [Header("Settings")]
+    public bool timeScaleIndependant = true;
+
+    [Header("Behaviour")]
+    public EnableBehaviour onEnable = EnableBehaviour.Restart;
+    public DisableBehaviour onDisable = DisableBehaviour.Stop;
+
+
+    [Header("Animation")]
+    public float cycleDuration = 1;
+    public float maxSize = 1.15f;
+    public float minSize = 0.85f;
+    public Ease ease = Ease.InOutSine;
+
+    public enum EnableBehaviour { Nothing, Resume, Restart }
+    public enum DisableBehaviour { Nothing, Pause, Stop }
+
+    private Tween tween;
+    private Transform tr;
+    private float normalScale = 1;
+
+    void Awake()
+    {
+        tr = transform;
+        normalScale = tr.localScale.x;
+    }
+
+    void OnEnable()
+    {
+        switch (onEnable)
+        {
+            case EnableBehaviour.Resume:
+                Play();
+                break;
+            case EnableBehaviour.Restart:
+                Restart();
+                break;
+        }
+    }
+
+    void OnDisable()
+    {
+        switch (onDisable)
+        {
+            case DisableBehaviour.Pause:
+                Pause();
+                break;
+            case DisableBehaviour.Stop:
+                Stop();
+                break;
+        }
+    }
+
+    public bool IsPlaying => IsTweenActive && tween.IsPlaying();
+    private bool IsTweenActive => tween != null && tween.IsActive();
+
+    public void StartAnimation()
+    {
+        KillTween();
+
+        tr.localScale = Vector3.one * minSize;
+
+        float duration = Mathf.Max(0.02f, cycleDuration / 2);
+        tween = tr.DOScale(maxSize, duration)
+            .SetEase(ease)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetUpdate(timeScaleIndependant);
+        tween.Goto(duration / 2, true);
+    }
+
+    public void Stop()
+    {
+        KillTween();
+        tr.localScale = Vector3.one * normalScale;
+    }
+
+    public void Restart()
+    {
+        if (IsTweenActive)
+            tween.Restart();
+        else
+            StartAnimation();
+    }
+
+    public void Pause()
+    {
+        if (IsPlaying)
+            tween.Pause();
+    }
+
+    public void Play()
+    {
+        if (IsTweenActive)
+            tween.Play();
+        else
+            StartAnimation();
+    }
+
+    void KillTween()
+    {
+        if (tween != null && tween.IsActive())
+            tween.Kill();
+        tween = null;
+    }
+
+}
+
