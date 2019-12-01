8aa0b2fa24ad73278332ce202da4359c2f67d0c7
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2019-12-01 12:38:44 -0500

    Added files to start making up the turn base system

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/_Proto_GridBattle/TurnSystem/SimTurnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimTurnManager : SimComponent
+{
+    public int DurationOfATurn = 10;
+    public Team StartTeam;
+
+    private Fix64 _timer = 0;
+    private Team _currentTeam;
+
+    private bool _hasInit = false;
+
+    public override void OnSimStart() 
+    {
+        _timer = DurationOfATurn;
+        _currentTeam = StartTeam;
+        _hasInit = true;
+        Debug.Log("SWITCH TURN - " + _currentTeam);
+    }
+
+    void Update()
+    {
+        if (_hasInit)
+        {
+            _timer -= (Fix64)Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                SwitchTurn();
+                _timer = DurationOfATurn;
+            }
+        }
+    }
+
+    private void SwitchTurn()
+    {
+        _currentTeam = _currentTeam + 1;
+        if((int)_currentTeam >= SimTeams.TEAM_COUNT)
+        {
+            _currentTeam = 0;
+        }
+        Debug.Log("SWITCH TURN - " + _currentTeam);
+    }
+}
---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/_Proto_GridBattle/TurnSystem/Teams.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Team
+{
+    Player,
+    AI
+}
+
+public class SimTeams
+{
+    public const int TEAM_COUNT = 2;
+}
+
+public class SimTeamMember : SimComponent
+{
+    public Team Team;
+}
