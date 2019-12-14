6403f3d940defc7ae331e7a79e1843c2181042cb
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2019-12-14 12:33:46 -0500

    File creation for Inventory system

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/_Proto_GridBattle/Inventory/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Inventory
+{
+    public List<InventoryItem> Items = new List<InventoryItem>();
+    public UnityEvent OnInventoryChanged = new UnityEvent();
+
+    public int AddItem(InventoryItem item, int position = -1)
+    {
+        if (position >= 0)
+        {
+            Items.Add(item);
+            OnInventoryChanged?.Invoke();
+            return Items.Count - 1;
+        }
+        else
+        {
+            Items[position] = item;
+            OnInventoryChanged?.Invoke();
+            return position;
+        }
+    }
+
+    public InventoryItem RemoveItem(int position)
+    {
+        InventoryItem itemRemoved = Items[position];
+        Items[position] = null;
+        OnInventoryChanged?.Invoke();
+        return itemRemoved;
+    }
+
+    public InventoryItem GetItem(int position)
+    {
+        return Items[position];
+    }
+
+    // todo
+    // could Add GetAllItem , to get all identical item we have in the inventory
+    // sort items
+    // stacking
+    // inventory size
+}
---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/_Proto_GridBattle/Inventory/SimInventoryContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimInventoryContainer : SimComponent
+{
+    public Inventory Inventory;
+
+    // multiple bags or multiple inventory here
+
+    public void TakeItem(GameObject item)
+    {
+
+    }
+}
