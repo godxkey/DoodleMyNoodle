4fe8530fa9792cf1c7aa4516f3a2c6a08f9cacd8
Frédéric Bessette <msfredb7@gmail.com>
2020-03-08 19:12:28 -0400

    New Bow item

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Components/SimDamageOnContactComponent.cs
@@ -0,0 +1,59 @@
+﻿using CCC.InspectorDisplay;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimDamageOnContactComponent : SimComponent, ISimTickable
+{
+    [System.Serializable]
+    struct SerializedData
+    {
+        public int Damage;
+        public bool DestroyOnContact;
+    }
+
+    public void OnSimTick()
+    {
+        SimTileId myTileId = SimTransform.GetTileId();
+
+        foreach (SimPawnComponent pawn in Simulation.EntitiesWithComponent<SimPawnComponent>())
+        {
+            // are we on the same tile as the pawn ?
+            if (pawn.SimTransform.GetTileId() == myTileId && pawn.SimEntity != SimEntity)
+            {
+                // damage pawn if possible
+                if(pawn.TryGetComponent(out SimHealthStatComponent pawnHealth))
+                {
+                    pawnHealth.IncreaseValue(-_data.Damage);
+                }
+
+                // destroy self
+                Simulation.Destroy(SimEntity);
+
+                break;
+            }
+        }
+    }
+
+    #region Serialized Data Methods
+    [UnityEngine.SerializeField]
+    [AlwaysExpand]
+    SerializedData _data = new SerializedData()
+    {
+        // define default values here
+        DestroyOnContact = true
+    };
+
+    public override void PushToDataStack(SimComponentDataStack dataStack)
+    {
+        base.PushToDataStack(dataStack);
+        dataStack.Push(_data);
+    }
+
+    public override void PopFromDataStack(SimComponentDataStack dataStack)
+    {
+        _data = (SerializedData)dataStack.Pop();
+        base.PopFromDataStack(dataStack);
+    }
+    #endregion
+}
---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Components/SimVelocityComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimVelocityComponent : SimComponent, ISimTickable
+{
+    [System.Serializable]
+    struct SerializedData
+    {
+        public FixVector2 Velocity;
+    }
+
+    public FixVector2 Value { get => _data.Velocity; set => _data.Velocity = value; }
+
+    public void OnSimTick()
+    {
+        SimTransform.LocalPosition += _data.Velocity * Simulation.DeltaTime;
+    }
+
+    #region Serialized Data Methods
+    [UnityEngine.SerializeField]
+    [CCC.InspectorDisplay.AlwaysExpand]
+    SerializedData _data = new SerializedData()
+    {
+        // define default values here
+    };
+
+    public override void PushToDataStack(SimComponentDataStack dataStack)
+    {
+        base.PushToDataStack(dataStack);
+        dataStack.Push(_data);
+    }
+
+    public override void PopFromDataStack(SimComponentDataStack dataStack)
+    {
+        _data = (SerializedData)dataStack.Pop();
+        base.PopFromDataStack(dataStack);
+    }
+    #endregion
+}
