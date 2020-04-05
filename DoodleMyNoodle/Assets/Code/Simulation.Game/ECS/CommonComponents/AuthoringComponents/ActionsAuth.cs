6e81a1c4219897e7ff9f0399eeb740cc2874f234
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-04-05 13:04:39 -0400

    Adding Action System

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/ActionsAuth.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ActionsAuth : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int StartValue = 4;
+    public int MaxValue = 4;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new ActionPoints { Value = StartValue });
+        dstManager.AddComponentData(entity, new MinimumInt<ActionPoints> { Value = 0 });
+        dstManager.AddComponentData(entity, new MaximumInt<ActionPoints> { Value = MaxValue });
+    }
+}
---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/Description.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+
+[Serializable]
+public struct Description : IComponentData
+{
+    // MAXIMUM 62 CHARACTERS !!!
+    public NativeString128 Value;
+}
