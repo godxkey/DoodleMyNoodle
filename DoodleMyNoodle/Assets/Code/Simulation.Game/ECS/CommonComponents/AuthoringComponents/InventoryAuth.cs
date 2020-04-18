3d0bce771717089daa36059ff2dbef49d025fcea
Frédéric Bessette <msfredb7@gmail.com>
2020-04-18 12:41:59 -0400

    cleanup

--- DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/InventoryAuth.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/InventoryAuth.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [DisallowMultipleComponent]
 [RequiresEntityConversion]
@@ -10,47 +9,14 @@ public class InventoryAuth : MonoBehaviour, IConvertGameObjectToEntity, IDeclare
 {
     public List<GameObject> InitialItems = new List<GameObject>();
 
-    private void Awake()
-    {
-        //for (int i = 0; i < InitialItems.Count; i++)
-        //{
-        //    InitialItems[i] = GameObject.Instantiate(InitialItems[i]);
-        //}
-    }
-
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        //List<Entity> itemEntities = new List<Entity>(InitialItems.Count);
-
-        //foreach (GameObject itemGO in InitialItems)
-        //{
-        //    itemEntities.Add(dstManager.Instantiate(conversionSystem.GetPrimaryEntity(itemGO)));
-        //}
-
         DynamicBuffer<StartingInventoryItem> inventory = dstManager.AddBuffer<StartingInventoryItem>(entity);
 
         foreach (GameObject itemGO in InitialItems)
-        //foreach (Entity itemEntity in itemEntities)
         {
             inventory.Add(new StartingInventoryItem() { ItemEntityPrefab = conversionSystem.GetPrimaryEntity(itemGO) });
-
-            //inventory.Add(new InventoryItemReference()
-            //{
-            //    ItemEntity = itemEntity
-            //});
         }
-        //foreach (GameObject itemGO in InitialItems)
-        //{
-        //    Entity itemEntity = conversionSystem.GetPrimaryEntity(itemGO);
-        //    conversionSystem.CreateAdditionalEntity(itemGO);
-        //    conversionSystem.CreateAdditionalEntity(itemGO);
-        //    conversionSystem.CreateAdditionalEntity(itemGO);
-
-        //    inventory.Add(new InventoryItemReference()
-        //    {
-        //        ItemEntity = itemEntity
-        //    });
-        //}
     }
 
     public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
