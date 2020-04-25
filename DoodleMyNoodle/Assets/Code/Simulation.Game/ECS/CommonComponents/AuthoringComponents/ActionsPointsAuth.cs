f62ed68adee58de4fa95064ee1c57ddff5ca5e95
Frédéric Bessette <msfredb7@gmail.com>
2020-04-25 13:54:39 -0400

    enemy can now attack

--- DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/ActionsPointsAuth.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/ActionsPointsAuth.cs
@@ -1,16 +1,25 @@
-﻿using Unity.Entities;
+﻿using CCC.InspectorDisplay;
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 [RequiresEntityConversion]
-public class ActionsAuth : MonoBehaviour, IConvertGameObjectToEntity
+public class ActionsPointsAuth : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public int StartValue = 4;
     public int MaxValue = 4;
+    public bool StartGameAtMaxValue = true;
+
+    [HideIf(nameof(StartGameAtMaxValue), HideShowBaseAttribute.Type.Field)]
+    public int StartValue = 4;
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
+        if (StartGameAtMaxValue)
+        {
+            StartValue = MaxValue;
+        }
+
         dstManager.AddComponentData(entity, new ActionPoints { Value = StartValue });
         dstManager.AddComponentData(entity, new MinimumInt<ActionPoints> { Value = 0 });
         dstManager.AddComponentData(entity, new MaximumInt<ActionPoints> { Value = MaxValue });
