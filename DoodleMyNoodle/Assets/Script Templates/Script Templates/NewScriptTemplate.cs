9a35eff23c11ece84fc93d435fec2d812172ab23
Frédéric Bessette <msfredb7@gmail.com>
2020-05-23 15:33:14 -0400

    New "Smart C# Script Template tool"

---
+++ DoodleMyNoodle/Assets/Script Templates/Script Templates/NewScriptTemplate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class NewScriptTemplate : ScriptTemplate
+{
+    public override string GetScriptContent()
+    {
+        return
+$@"using System;
+using UnityEngine;
+
+
+public class #SCRIPTNAME# : ScriptTemplate
+{{
+    public override string GetScriptContent()
+    {{
+        // When refering to a type, it is recommended to use {{nameof(TheType)}}.
+        // This helps keeping the templates up-to-date when types are renamed or removed.
+
+        return
+$@""using {{nameof({nameof(System)})}};
+using {{nameof({nameof(UnityEngine)})}};
+
+public class #%STRIPME%SCRIPTNAME%STRIPME%# : {{nameof({nameof(MonoBehaviour)})}}
+{{{{
+    void Start()
+    {{{{
+
+    }}}}
+    
+    void Update()
+    {{{{
+
+    }}}}
+}}}}"";
+    }}
+
+    public override string GetScriptDefaultName()
+    {{
+        return ""NewScriptTemplate"";
+    }}
+}}";
+    }
+
+    public override string GetScriptDefaultName()
+    {
+        return "NewScriptTemplate";
+    }
+}
