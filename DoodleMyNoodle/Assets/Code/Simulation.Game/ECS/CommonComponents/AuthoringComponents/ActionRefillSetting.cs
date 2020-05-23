240bf0fbe9761efa4e7dc3127ab757717c28d86e
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-05-23 11:51:16 -0400

    Action Refill system

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/ActionRefillSetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ActionRefillSetting : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int AmountToAdd = 0;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new ActionRefillAmount { Value = AmountToAdd });
+    }
+}
---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/ComponentSystems/ActionRefillSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class ActionRefillSystem : SimComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        if (HasSingleton<NewTurnEventData>())
+        {
+            int currentTeam = CommonReads.GetCurrentTurnTeam(Accessor);
+
+            Entities
+                .ForEach((Entity controller, ref Team team, ref ActionPoints pawnActionPoints, ref MaximumInt<ActionPoints> pawnMaximumActionPoints) =>
+                {
+                    if ((team.Value != currentTeam) || !HasSingleton<ActionRefillAmount>())
+                    {
+                        return;
+                    }
+
+                    int actionPointsToAdd = GetSingleton<ActionRefillAmount>().Value;
+
+                    pawnActionPoints.Value = Mathf.Clamp(pawnActionPoints.Value + actionPointsToAdd, 0, pawnMaximumActionPoints.Value);
+                });
+        }
+    }
+}
