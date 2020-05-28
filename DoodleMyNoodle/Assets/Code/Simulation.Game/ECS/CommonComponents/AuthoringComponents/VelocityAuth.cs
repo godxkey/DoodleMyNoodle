0c873b5ae0ea383f49263d72a17d01761db21136
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-05-28 18:24:07 -0400

    Velocity Auth component changed

---
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/ECS/CommonComponents/AuthoringComponents/VelocityAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+[Serializable]
+public struct Velocity : IComponentData
+{
+    public fix3 Value;
+}
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class VelocityAuth : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Velocity { Value = new fix3(0,0,0) });
+        dstManager.AddComponentData(entity, new PotentialNewTranslation { Value = new fix3(0, 0, 0) });
+    }
+}
