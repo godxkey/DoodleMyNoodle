f948b5595b7cdbb452f18f8e4b75af4897217b2b
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-06-23 14:53:49 -0400

    working side scroller map with new brush

---
+++ DoodleMyNoodle/Assets/Code/Tools/Grid/SmartBrush.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace UnityEditor.Tilemaps
+{
+    [CreateAssetMenu]
+    [CustomGridBrush(false, true, false, "Smart Brush")]
+    public class SmartBrush : BaseSmartBrush
+    {
+        public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pivot)
+        {
+            base.Pick(gridLayout, brushTarget, position, pivot);
+
+            Debug.Log("Layer : " + brushTarget.name);
+            Debug.Log("Layout : " + gridLayout.name);
+
+            foreach (var target in GridPaintingState.validTargets)
+            {
+                Debug.Log("Target : " + target.name);
+            }
+        }
+    }
+
+    [CustomEditor(typeof(SmartBrush))]
+    public class SmartBrushEditor : BaseSmartBrushEditor
+    {
+    }
+}
