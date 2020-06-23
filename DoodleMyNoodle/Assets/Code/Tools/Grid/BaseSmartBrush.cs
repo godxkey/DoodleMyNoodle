a2f6d74e0a32771a7804d97bb475a9b17ed39981
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-06-23 16:41:37 -0400

    SmartBrush fixes and map setup + sprite improvements

---
+++ DoodleMyNoodle/Assets/Code/Tools/Grid/BaseSmartBrush.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace UnityEditor.Tilemaps
+{
+    //public class BaseSmartBrush : GridBrushBase
+    //{
+    //    protected Sprite GetCurrentSelectedSprite(GameObject brushTarget, Vector3Int position)
+    //    {
+    //        UnityEngine.Tilemaps.Tilemap currentTileMap = brushTarget.GetComponent<UnityEngine.Tilemaps.Tilemap>();
+    //        Tile currentlySelectedTile = currentTileMap.GetTile(position) as Tile;
+    //        return currentlySelectedTile.sprite;
+    //    }
+
+    //    protected List<GameObject> GetObjectsInCell(GridLayout grid, Transform parent, Vector3Int position)
+    //    {
+    //        var results = new List<GameObject>();
+    //        var childCount = parent.childCount;
+    //        for (var i = 0; i < childCount; i++)
+    //        {
+    //            var child = parent.GetChild(i);
+    //            if (position == grid.WorldToCell(child.position))
+    //            {
+    //                results.Add(child.gameObject);
+    //            }
+    //        }
+    //        return results;
+    //    }
+    //}
+
+    //[CustomEditor(typeof(BaseSmartBrush))]
+    //public class BaseSmartBrushEditor : GridBrushEditor
+    //{
+    //    public override GameObject[] validTargets
+    //    {
+    //        get
+    //        {
+    //            return FindObjectsOfType<Tilemap>().Select(x => x.gameObject).ToArray();
+    //        }
+    //    }
+    //}
+}
--- DoodleMyNoodle/Assets/Code/Tools/Grid/SmartBrush.cs
+++ DoodleMyNoodle/Assets/Code/Tools/Grid/SmartBrush.cs
@@ -1,28 +1,76 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace UnityEditor.Tilemaps
 {
     [CreateAssetMenu]
     [CustomGridBrush(false, true, false, "Smart Brush")]
-    public class SmartBrush : BaseSmartBrush
+    public class SmartBrush : GridBrush
     {
+        public enum TileMapLayers
+        {
+            Background,
+            LevelContent,
+            Foreground
+        }
+
+        [System.Serializable]
+        public struct SpriteLayerData
+        {
+            public Sprite Sprite;
+            public TileMapLayers Layer;
+        }
+
+        public List<SpriteLayerData> SpriteLayers = new List<SpriteLayerData>();
+
         public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pivot)
         {
-            base.Pick(gridLayout, brushTarget, position, pivot);
-
-            Debug.Log("Layer : " + brushTarget.name);
-            Debug.Log("Layout : " + gridLayout.name);
-
-            foreach (var target in GridPaintingState.validTargets)
+            GameObject newLayer = FindLayerObject(FindLayerFromSprite(GetCurrentSelectedSprite(brushTarget, position)));
+            if(newLayer != null)
             {
-                Debug.Log("Target : " + target.name);
+                GridPaintingState.scenePaintTarget = newLayer;
             }
+
+            base.Pick(gridLayout, brushTarget, position, pivot);
+        }
+
+        private GameObject FindLayerObject(TileMapLayers layer)
+        {
+            foreach (GameObject target in GridPaintingState.validTargets)
+            {
+                string test = layer.ToString();
+                if (layer.ToString() == target.name)
+                {
+                    return target;
+                }
+            }
+
+            return null;
+        }
+
+        private TileMapLayers FindLayerFromSprite(Sprite sprite)
+        {
+            foreach (SpriteLayerData spriteLayerData in SpriteLayers)
+            {
+                if(spriteLayerData.Sprite == sprite)
+                {
+                    return spriteLayerData.Layer;
+                }
+            }
+
+            Debug.LogError("Sprite not found for SmartBrush");
+            return TileMapLayers.Background;
+        }
+
+        private Sprite GetCurrentSelectedSprite(GameObject brushTarget, BoundsInt position)
+        {
+            UnityEngine.Tilemaps.Tilemap currentTileMap = brushTarget.GetComponent<UnityEngine.Tilemaps.Tilemap>();
+            Tile currentlySelectedTile = currentTileMap.GetTile(position.position) as Tile;
+            return currentlySelectedTile.sprite;
         }
     }
 
     [CustomEditor(typeof(SmartBrush))]
-    public class SmartBrushEditor : BaseSmartBrushEditor
-    {
-    }
+    public class SmartBrushEditor : GridBrushEditor { }
 }
