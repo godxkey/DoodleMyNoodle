bb62abfacea419b44b166239f80254ce601033d7
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-08-08 14:01:17 -0400

    fix for chest that are getting deleted

--- DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Inventory/DestroyEmptyInteractableInventorySystem.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Inventory/DestroyEmptyInteractableInventorySystem.cs
@@ -8,7 +8,8 @@ public class DestroyEmptyInteractableInventorySystem : SimComponentSystem
     protected override void OnUpdate()
     {
         Entities
-        .WithAll<Interactable, Interacted>()
+        .WithAll<Interactable>()
+        .WithNone<StartingInventoryItem>()
         .ForEach((Entity interactable, ref FixTranslation pos, DynamicBuffer<InventoryItemReference> inventory) =>
         {
             if (inventory.Length < 1)
