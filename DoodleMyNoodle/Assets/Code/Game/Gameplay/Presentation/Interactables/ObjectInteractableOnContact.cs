ffbe6c259d04c073650bcbab301317b04c307d0c
Frédéric Bessette <msfredb7@gmail.com>
2020-08-22 13:23:48 -0400

    fix errors when no pawn

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Interactables/ObjectInteractableOnContact.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Interactables/ObjectInteractableOnContact.cs
@@ -8,13 +8,16 @@ public class ObjectInteractableOnContact : InteractableEntityView
 {
     protected override void OnGamePresentationUpdate() 
     {
-        int2 localPlayerTile = Helpers.GetTile(SimWorld.GetComponentData<FixTranslation>(SimWorldCache.LocalPawn));
-        int2 currentInteractableTile = Helpers.GetTile(SimWorld.GetComponentData<FixTranslation>(SimEntity));
-
-        if (((localPlayerTile.x == currentInteractableTile.x) && (localPlayerTile.y == currentInteractableTile.y)) && (CanTrigger()))
+        if(SimWorldCache.LocalPawn != Entity.Null)
         {
-            SimPlayerInputUseInteractable inputUseInteractable = new SimPlayerInputUseInteractable(transform.position);
-            SimWorld.SubmitInput(inputUseInteractable);
+            int2 localPlayerTile = SimWorldCache.LocalPawnTile;
+            int2 currentInteractableTile = Helpers.GetTile(SimWorld.GetComponentData<FixTranslation>(SimEntity));
+
+            if ((localPlayerTile.x == currentInteractableTile.x) && (localPlayerTile.y == currentInteractableTile.y) && CanTrigger())
+            {
+                SimPlayerInputUseInteractable inputUseInteractable = new SimPlayerInputUseInteractable(transform.position);
+                SimWorld.SubmitInput(inputUseInteractable);
+            }
         }
     }
 }
