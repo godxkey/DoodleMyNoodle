a91d29f9ab5bc1d5103c0674a7694e889f00b531
Frédéric Bessette <msfredb7@gmail.com>
2020-08-22 16:37:39 -0400

    delete grid battle

--- DoodleMyNoodle/Assets/Code/Tools/SceneShortcuts.generated.cs
+++ DoodleMyNoodle/Assets/Code/Tools/SceneShortcuts.generated.cs
@@ -37,31 +37,25 @@ public static class SceneShortcuts
     [MenuItem("Scene Shortcuts/Gym_Physics", priority = 4009)]
     public static void Assets_Scenes_Levels_Scene_Assets_Gym_Physics() => LoadScene("Assets/Scenes/Levels/Scene Assets/Gym_Physics.unity");
 
-    [MenuItem("Scene Shortcuts/Lvl_GridBattle", priority = 4010)]
-    public static void Assets_Scenes_Levels_Scene_Assets_Lvl_GridBattle() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_GridBattle.unity");
-
-    [MenuItem("Scene Shortcuts/Lvl_GridBattle_Presentation", priority = 4011)]
-    public static void Assets_Scenes_Levels_Scene_Assets_Lvl_GridBattle_Presentation() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_GridBattle_Presentation.unity");
-
-    [MenuItem("Scene Shortcuts/Lvl_Prototype3", priority = 4012)]
+    [MenuItem("Scene Shortcuts/Lvl_Prototype3", priority = 4010)]
     public static void Assets_Scenes_Levels_Scene_Assets_Lvl_Prototype3() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_Prototype3.unity");
 
-    [MenuItem("Scene Shortcuts/Lvl_SideScrollerExemple", priority = 4013)]
+    [MenuItem("Scene Shortcuts/Lvl_SideScrollerExemple", priority = 4011)]
     public static void Assets_Scenes_Levels_Scene_Assets_Lvl_SideScrollerExemple() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_SideScrollerExemple.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_InGameEscape", priority = 5014)]
+    [MenuItem("Scene Shortcuts/Menu_InGameEscape", priority = 5012)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_InGameEscape() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_InGameEscape.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineClientSessionChoice", priority = 5015)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineClientSessionChoice", priority = 5013)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineClientSessionChoice() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineClientSessionChoice.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineRoleChoice", priority = 5016)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineRoleChoice", priority = 5014)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineRoleChoice() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineRoleChoice.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineServerSessionCreation", priority = 5017)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineServerSessionCreation", priority = 5015)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineServerSessionCreation() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineServerSessionCreation.unity");
 
-    [MenuItem("Scene Shortcuts/Misc_LoadingScreen", priority = 6018)]
+    [MenuItem("Scene Shortcuts/Misc_LoadingScreen", priority = 6016)]
     public static void Assets_Scenes_Misc_Scene_Assets_Misc_LoadingScreen() => LoadScene("Assets/Scenes/Misc/Scene Assets/Misc_LoadingScreen.unity");
 
 
