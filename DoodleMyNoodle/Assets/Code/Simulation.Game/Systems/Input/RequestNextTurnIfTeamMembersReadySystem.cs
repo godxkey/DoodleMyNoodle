da67e28a5aa6471517c96506964119bb8ee5d92b
Frédéric Bessette <msfredb7@gmail.com>
2020-09-13 18:40:03 -0400

    missing file

--- DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Input/RequestNextTurnIfTeamMembersReadySystem.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Input/RequestNextTurnIfTeamMembersReadySystem.cs
@@ -20,10 +20,10 @@ public class RequestNextTurnIfTeamMembersReadySystem : SimComponentSystem
         int teamCurrentlyPlaying = GetSingleton<TurnCurrentTeamSingletonComponent>().Value;
         bool everyoneIsReady = true;
 
-        Entities.ForEach((ref Team team, ref ReadyForNextTurn readyForNextTurn) =>
+        Entities.ForEach((ref Active active, ref ControlledEntity pawn, ref Team team, ref ReadyForNextTurn readyForNextTurn) =>
         {
             // if a team member is NOT ready
-            if (team.Value == teamCurrentlyPlaying && !readyForNextTurn.Value)
+            if (active && EntityManager.Exists(pawn) && team == teamCurrentlyPlaying && !readyForNextTurn.Value)
             {
                 everyoneIsReady = false;
             }
