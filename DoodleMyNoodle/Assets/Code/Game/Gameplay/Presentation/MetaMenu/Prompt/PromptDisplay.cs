b646b4f47e7800127337ca6cb8fba69429b7024b
Frédéric Bessette <msfredb7@gmail.com>
2020-09-26 13:22:14 -0400

    small clean up in prompt display: - fix anchoring issue with flat screens - clean up code a little - rename prefabs to clearer names

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/Prompt/PromptDisplay.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/Prompt/PromptDisplay.cs
@@ -6,36 +6,39 @@ using UnityEngineX;
 public class PromptDisplay : GameSystem<PromptDisplay>
 {
     [SerializeField] private TextMeshProUGUI _questionText;
-    [SerializeField] private GameObject _answerButtonPrefab;
+    [SerializeField] private PromptAnswerDisplay _answerButtonPrefab;
     [SerializeField] private Transform _answersContainer;
-    [SerializeField] private GameObject _blackScreen;
 
     public override bool SystemReady => true;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        gameObject.SetActive(false);
+    }
 
     public void Ask(string question, Action<int> onAnswerSelected, params string[] answers)
     {
         ClearAnswers();
+
+        gameObject.SetActive(true);
 
         _questionText.text = question;
 
         for (int i = 0; i < answers.Length; i++)
         {
             string answer = answers[i];
-            PromptAnswerDisplay promptAnswerDisplay = Instantiate(_answerButtonPrefab, _answersContainer).GetComponent<PromptAnswerDisplay>();
-            if(promptAnswerDisplay != null)
-            {
-                int choiceIndex = i;
-                promptAnswerDisplay.Init(answer, () => 
-                {
-                    onAnswerSelected(choiceIndex);
-                    _blackScreen?.SetActive(false);
-                });
-            }
-        }
+            PromptAnswerDisplay promptAnswerDisplay = Instantiate(_answerButtonPrefab, _answersContainer);
 
-        _blackScreen?.SetActive(true);
+            int choiceIndex = i;
+            promptAnswerDisplay.Init(answer, () =>
+            {
+                onAnswerSelected(choiceIndex);
+                gameObject.SetActive(false);
+            });
+        }
     }
-    
+
     private void ClearAnswers()
     {
         PromptAnswerDisplay[] answers = _answersContainer.GetComponentsInChildren<PromptAnswerDisplay>();
