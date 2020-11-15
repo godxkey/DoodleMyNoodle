cc559249eaf981e1ca37796aa23899d8ad94e5a1
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2020-11-15 12:21:01 -0500

    Camera tweak, ping system, lobby qol

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/LobbyOverview/LobbyPlayerInfoDisplay.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/LobbyOverview/LobbyPlayerInfoDisplay.cs
@@ -1,17 +1,32 @@
 using System;
 using TMPro;
 using Unity.Collections;
+using Unity.Entities;
 using UnityEngine;
 using UnityEngineX;
 
-public class LobbyPlayerInfoDisplay : MonoBehaviour
+public class LobbyPlayerInfoDisplay : GamePresentationBehaviour
 {
     [SerializeField] private TextMeshProUGUI _playerName;
     [SerializeField] private TextMeshProUGUI _characterName;
 
-    public void UpdatePlayerInfoDisplay(FixedString64 playerName, FixedString64 characterName)
+    private Entity _currentPlayer;
+
+    public void UpdatePlayerInfoDisplay(FixedString64 playerName, FixedString64 characterName, Entity pawnReference)
     {
         _playerName.text = playerName.ToString();
         _characterName.text = characterName.ToString();
+
+        _currentPlayer = pawnReference;
     }
+
+    public void OnPlayerInfoClicked()
+    {
+        if (SimWorld.TryGetComponentData(_currentPlayer, out FixTranslation translation))
+        {
+            CameraMovementController.Instance.TeleportCameraToPosition(translation.Value.ToUnityVec());
+        }
+    }
+
+    protected override void OnGamePresentationUpdate() { }
 }
