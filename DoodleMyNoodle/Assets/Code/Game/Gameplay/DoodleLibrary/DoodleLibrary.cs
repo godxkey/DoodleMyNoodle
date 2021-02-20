4c2520cd902b74accfdf933e0acd7bf307f4c5b5
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2021-02-20 09:34:01 -0500

    refactor character creation

---
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/DoodleLibrary/DoodleLibrary.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngineX;
+
+[CreateAssetMenu(menuName = "DoodleLibrary")]
+public class DoodleLibrary : FileScriptableInterface
+{
+    private const string _doodleFileName = "doodle";
+    private const string _extension = ".png";
+
+    [Serializable]
+    public class DoodleLibraryData
+    {
+        public int TotalDoodleCount = 0;
+        public int LastDoodleUsedIndex = -1;
+    }
+
+    private DoodleLibraryData _data = new DoodleLibraryData();
+
+    public Texture2D GetLastDoodle()
+    {
+        return GetDoodle(_data.LastDoodleUsedIndex);
+    }
+
+    public List<Texture2D> GetAllDoodles()
+    {
+        List<Texture2D> resultTextures = new List<Texture2D>();
+
+        for (int i = 0; i < _data.TotalDoodleCount; i++)
+        {
+            resultTextures.Add(GetDoodle(i));
+        }
+
+        return resultTextures;
+    }
+
+    public void AddDoodle(Texture2D doodleTexture, bool SetAsLast = false)
+    {
+        _data.TotalDoodleCount++;
+        if (SetAsLast)
+        {
+            _data.LastDoodleUsedIndex = _data.TotalDoodleCount - 1;
+        }
+
+        SaveDoodle(doodleTexture);
+    }
+
+    private void SaveDoodle(Texture2D doodleTexture)
+    {
+        byte[] result = doodleTexture.EncodeToPNG();
+
+        try
+        {
+            File.WriteAllBytes(GetDoodlePath(_data.TotalDoodleCount - 1), result);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Could not export and save doodle in library");
+        }
+    }
+
+    private Texture2D GetDoodle(int index)
+    {
+        Texture2D resultTexture = null;
+        if (File.Exists(GetDoodlePath(index)))
+        {
+            try
+            {
+                byte[] bytes = File.ReadAllBytes(GetDoodlePath(index));
+                resultTexture.LoadImage(bytes);
+            }
+            catch (Exception e)
+            {
+                Log.Info($"Cannot import texture at path {GetDoodlePath(index)}: {e.Message}");
+            }
+        }
+
+        return resultTexture;
+    }
+
+    private string GetDoodlePath(int index)
+    {
+        string fullPath = Application.persistentDataPath;
+        fullPath = fullPath.Replace('/', '\\');
+
+        if (!fullPath.EndsWith("\\"))
+            fullPath += "\\";
+
+        fullPath += _doodleFileName + index + _extension;
+
+        return fullPath;
+    }
+
+    protected override object GetLocalData()
+    {
+        return _data;
+    }
+
+    protected override void OverwriteLocalData(object graph)
+    {
+        _data = (DoodleLibraryData)graph;
+    }
+
+    protected override void SetDefaultLocalData()
+    {
+        _data = new DoodleLibraryData();
+        _data.LastDoodleUsedIndex = -1;
+        _data.TotalDoodleCount = 0;
+    }
+}
--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/CharacterCreation/CharacterCreationScreen.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/MetaMenu/CharacterCreation/CharacterCreationScreen.cs
@@ -8,6 +8,7 @@ using UnityEngineX;
 
 public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationScreen>
 {
+    [SerializeField] private DoodleLibrary _library;
     [SerializeField] private GameObject _screenContainer;
     [SerializeField] private Button _readyButton;
     [SerializeField] private Toggle _characterIsLookingRightToggle;
@@ -15,31 +16,6 @@ public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationS
 
     private PlayerDoodleAsset _doodleAsset;
     private bool _settingsApplied = false;
-
-    private string PawnNamePref
-    {
-        get
-        {
-            return PlayerPrefs.GetString("pawn-name", defaultValue: "");
-        }
-        set
-        {
-            PlayerPrefs.SetString("pawn-name", value);
-            PlayerPrefs.Save();
-        }
-    }
-    private string DoodlePathPref
-    {
-        get
-        {
-            return PlayerPrefs.GetString("doodle-path-2", defaultValue: "");
-        }
-        set
-        {
-            PlayerPrefs.SetString("doodle-path-2", value);
-            PlayerPrefs.Save();
-        }
-    }
 
     protected override void Awake()
     {
@@ -50,16 +26,12 @@ public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationS
     {
         base.OnGameStart();
 
-
         _readyButton.onClick.AddListener(ApplyCharacterSettings);
 
-        // import previous doodle
         _doodleAsset = PlayerAssetManager.Instance.CreateAsset<PlayerDoodleAsset>();
-        ImportDoodle(DoodlePathPref);
-
         _uPaint.Initialize(new Vector2Int(512, 512), FilterMode.Point, 10);
         _uPaint.AddLayer();
-        //_uPaint.ImportImage(_doodleAsset.Texture);
+        Import();
     }
 
     protected override void OnDestroy()
@@ -72,26 +44,6 @@ public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationS
     protected override void OnGamePresentationUpdate()
     {
         _screenContainer.SetActive(!SimWorld.HasSingleton<GameStartedTag>());
-    }
-
-    private void ImportDoodle(string path)
-    {
-        // TODO : LIBRARY SYSTEM HERE
-
-        if (File.Exists(path))
-        {
-            try
-            {
-                byte[] bytes = File.ReadAllBytes(path);
-
-                _doodleAsset.Load(bytes);
-                DoodlePathPref = path;
-            }
-            catch (Exception e)
-            {
-                Log.Info($"Cannot import doodle at path {path}: {e.Message}");
-            }
-        }
     }
 
     private void ApplyCharacterSettings()
@@ -112,8 +64,6 @@ public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationS
             // record pawn name for future use
             PromptDisplay.Instance.AskString("Enter your character name here :", (string characterName) =>
             {
-                PawnNamePref = characterName;
-
                 // Set name
                 SimPlayerInputSetPawnName startNameInput = new SimPlayerInputSetPawnName(characterName);
                 SimWorld.SubmitInput(startNameInput);
@@ -122,39 +72,21 @@ public class CharacterCreationScreen : GamePresentationSystem<CharacterCreationS
             });
 
             // We triggered everything, let's now save in the background while our things are done async
-            _uPaint.ExportToPNG();
+            Export();
+        }
+    }
+
+    private void Import()
+    {
+        Texture2D lastDoodle = _library.GetLastDoodle();
+        if (lastDoodle != null)
+        {
+            _doodleAsset.SetTexture(lastDoodle);
         }
     }
 
     private void Export()
     {
-        // TODO : Change for library system
-
-        try
-        {
-            File.WriteAllBytes(GetExportPath(), _uPaint.ExportToPNG());
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Could not export and save last doodle made in character creation screen");
-        }
-    }
-
-    private string GetExportPath()
-    {
-        // TODO : Change for library system
-
-        string fullPath = Application.persistentDataPath;
-        fullPath = fullPath.Replace('/', '\\');
-
-        if (!fullPath.EndsWith("\\"))
-            fullPath += "\\";
-
-        fullPath += "lastDoodle";
-
-        if (!fullPath.EndsWith(".png"))
-            fullPath += ".png";
-
-        return fullPath;
+        _library.AddDoodle(_uPaint.GetLayerTexture(0));
     }
 }
