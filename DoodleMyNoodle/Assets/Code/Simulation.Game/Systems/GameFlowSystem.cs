43a2a9a2fcf2018c6e4878b654053438c330c57e
Frédéric Bessette <msfredb7@gmail.com>
2021-05-15 15:12:16 -0400

    avoir des AI allié ne bloque pas le lancement de la partie

--- DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/GameFlowSystem.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/GameFlowSystem.cs
@@ -8,7 +8,7 @@ public struct CanTriggerGameOverTag : IComponentData { }
 
 public struct GameStartedTag : IComponentData { }
 
-public class GameFlowSystem : SimComponentSystem
+public class GameFlowSystem : SimSystemBase
 {
     protected override void OnUpdate()
     {
@@ -23,29 +23,34 @@ public class GameFlowSystem : SimComponentSystem
 
         int teamCurrentlyPlaying = CommonReads.GetTurnTeam(Accessor);
 
-        bool everyoneIsReady = true;
-        bool atLeastOnePlayerExists = false;
-
-        // check if every player is ready
-        Entities.ForEach((ref Team team, ref Active active, ref ReadyForNextTurn readyForNextTurn, ref ControlledEntity pawn) =>
-        {
-            if (active && team == (int)DesignerFriendlyTeam.Player)
-            {
-                atLeastOnePlayerExists = true;
-
-                if (!readyForNextTurn && EntityManager.Exists(pawn))
-                {
-                    everyoneIsReady = false; // if a team member is NOT ready
-                }
-            }
-        });
-
         // if a team member is NOT ready
-        if (teamCurrentlyPlaying == -1 && atLeastOnePlayerExists && everyoneIsReady)
+        if (teamCurrentlyPlaying == -1)
         {
-            this.SetOrCreateSingleton(new GameStartedTag());
+            bool everyoneIsReady = true;
+            bool atLeastOnePlayerExists = false;
 
-            CommonWrites.RequestNextTurn(Accessor);
+            // check if every player is ready
+            Entities
+                .WithNone<AITag>() // ignore AI
+                .ForEach((in Team team, in Active active, in ReadyForNextTurn readyForNextTurn, in ControlledEntity pawn) =>
+            {
+                if (active && team == (int)DesignerFriendlyTeam.Player)
+                {
+                    atLeastOnePlayerExists = true;
+
+                    if (!readyForNextTurn && HasComponent<Controllable>(pawn))
+                    {
+                        everyoneIsReady = false; // if a team member is NOT ready
+                    }
+                }
+            }).Run();
+
+            if (atLeastOnePlayerExists && everyoneIsReady)
+            {
+                SetOrCreateSingleton(new GameStartedTag());
+
+                CommonWrites.RequestNextTurn(Accessor);
+            }
         }
     }
 
@@ -58,7 +63,7 @@ public class GameFlowSystem : SimComponentSystem
             if (CommonReads.GetEntitiesFromTeam(Accessor, (int)DesignerFriendlyTeam.Baddies).Length > 0 &&
                 CommonReads.GetEntitiesFromTeam(Accessor, (int)DesignerFriendlyTeam.Player).Length > 0)
             {
-                this.CreateSingleton<CanTriggerGameOverTag>();
+                CreateSingleton<CanTriggerGameOverTag>();
             }
         }
 
@@ -68,10 +73,10 @@ public class GameFlowSystem : SimComponentSystem
             int playerAlive = 0;
             int aiAlive = 0;
 
-            Entities.ForEach((ref Team pawnControllerTeam, ref ControlledEntity pawn) =>
+            Entities.ForEach((in Team pawnControllerTeam, in ControlledEntity pawn) =>
             {
                 // if the team member controls a pawn with Health
-                if (EntityManager.Exists(pawn.Value) && EntityManager.HasComponent<Health>(pawn.Value))
+                if (HasComponent<Health>(pawn.Value))
                 {
                     if (pawnControllerTeam.Value == (int)DesignerFriendlyTeam.Baddies)
                     {
@@ -82,14 +87,14 @@ public class GameFlowSystem : SimComponentSystem
                         playerAlive++;
                     }
                 }
-            });
+            }).Run();
 
             if (aiAlive <= 0)
             {
                 // Player wins !
                 if (!HasSingleton<WinningTeam>())
                 {
-                    this.CreateSingleton(new WinningTeam { Value = (int)DesignerFriendlyTeam.Player });
+                    CreateSingleton(new WinningTeam { Value = (int)DesignerFriendlyTeam.Player });
                 }
             }
             else if (playerAlive <= 0)
@@ -97,7 +102,7 @@ public class GameFlowSystem : SimComponentSystem
                 // AI wins !
                 if (!HasSingleton<WinningTeam>())
                 {
-                    this.CreateSingleton(new WinningTeam { Value = (int)DesignerFriendlyTeam.Baddies });
+                    CreateSingleton(new WinningTeam { Value = (int)DesignerFriendlyTeam.Baddies });
                 }
             }
         }
