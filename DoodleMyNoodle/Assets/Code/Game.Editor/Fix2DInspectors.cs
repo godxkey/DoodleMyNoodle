1fa78be62497bc059d9b6bdadd60d4d621ca3cfc
Frédéric Bessette <msfredb7@gmail.com>
2021-07-11 14:16:01 -0400

    collider custom inspector

--- DoodleMyNoodle/Assets/Code/Game.Editor/Fix2DInspectors.cs
+++ DoodleMyNoodle/Assets/Code/Game.Editor/Fix2DInspectors.cs
@@ -1,8 +1,10 @@
 ﻿using CCC.Editor;
 using JetBrains.Annotations;
 using Unity.Properties;
+using Unity.Properties.UI;
 using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [UsedImplicitly]
 class Fix3Inspector : BaseFieldInspector<Vector3Field, Vector3, fix3>
@@ -31,5 +33,48 @@ class FixInspector : BaseFieldInspector<FloatField, float, fix>
     {
         TypeConversion.Register<fix, float>(v => (float)v);
         TypeConversion.Register<float, fix>(v => (fix)v);
+    }
+}
+
+[UsedImplicitly]
+class PhysicsColliderInspector : Inspector<CCC.Fix2D.PhysicsColliderBlob>
+{
+    private Toggle _isCreatedField;
+    private EnumField _typeField;
+    private FloatField _radiusField;
+    private LongField _addressField;
+
+    public override VisualElement Build()
+    {
+        var container = new VisualElement();
+
+        _isCreatedField = new Toggle("Is Created");
+        _addressField = new LongField("Address");
+        _typeField = new EnumField("Type", CCC.Fix2D.ColliderType.Invalid);
+        _radiusField = new FloatField("Radius");
+
+        container.Add(_isCreatedField);
+        container.Add(_addressField);
+        container.Add(_typeField);
+        container.Add(_radiusField);
+
+        return container;
+    }
+
+    public override void Update()
+    {
+        _isCreatedField.SetValueWithoutNotify(Target.Collider.IsCreated);
+
+        unsafe
+        {
+            long address = (long)Target.Collider.GetUnsafePtr();
+            _addressField.SetValueWithoutNotify(address);
+        }
+
+        if (Target.Collider.IsCreated)
+        {
+            _radiusField.SetValueWithoutNotify(Target.Collider.Value.Radius);
+            _typeField.SetValueWithoutNotify(Target.Collider.Value.ColliderType);
+        }
     }
 }
