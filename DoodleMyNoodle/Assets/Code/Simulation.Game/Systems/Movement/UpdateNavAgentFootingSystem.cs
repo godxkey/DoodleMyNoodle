c6aba015a104f3aaff58d5dbb06c5cf5bebaa7cd
Frédéric Bessette <msfredb7@gmail.com>
2021-10-09 17:07:05 -0400

    fix 'Grounded' not working on platforms

--- DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Movement/UpdateNavAgentFootingSystem.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/Systems/Movement/UpdateNavAgentFootingSystem.cs
@@ -1,22 +1,29 @@
 ﻿using CCC.Fix2D;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 
 [UpdateInGroup(typeof(MovementSystemGroup))]
 public class UpdateNavAgentFootingSystem : SimSystemBase
 {
+    private PhysicsWorldSystem _physicsWorldSystem;
+
     protected override void OnCreate()
     {
         base.OnCreate();
+        _physicsWorldSystem = World.GetOrCreateSystem<PhysicsWorldSystem>();
         RequireSingletonForUpdate<GridInfo>();
     }
 
     protected override void OnUpdate()
     {
         TileWorld tileWorld = CommonReads.GetTileWorld(Accessor);
+        PhysicsWorld physicsWorld = _physicsWorldSystem.PhysicsWorld;
+
         Entities
             .WithReadOnly(tileWorld)
-            .ForEach((ref NavAgentFootingState footing, in FixTranslation fixTranslation, in PhysicsColliderBlob colliderRef, in PhysicsVelocity velocity) =>
+            .WithReadOnly(physicsWorld)
+            .ForEach((Entity entity, ref NavAgentFootingState footing, in FixTranslation fixTranslation, in PhysicsColliderBlob colliderRef, in PhysicsVelocity velocity) =>
             {
                 ref Collider collider = ref colliderRef.Collider.Value;
                 fix pawnRadius = (fix)collider.Radius;
@@ -24,27 +31,42 @@ public class UpdateNavAgentFootingSystem : SimSystemBase
                 fix2 belowLeftFoot = new fix2(fixTranslation.Value.x - pawnFeetXOffset, fixTranslation.Value.y - pawnRadius - (fix)0.05);
                 fix2 belowRightFoot = new fix2(fixTranslation.Value.x + pawnFeetXOffset, fixTranslation.Value.y - pawnRadius - (fix)0.05);
 
+
                 // GOTO Ladder IF on ladder && (already has footing on ladder || already has footing on ground)
                 if (tileWorld.GetFlags(Helpers.GetTile(fixTranslation)).IsLadder
                     && (footing.Value == NavAgentFooting.Ladder || footing.Value == NavAgentFooting.Ground))
                 {
                     footing.Value = NavAgentFooting.Ladder;
                 }
-
-                // GOTO Ground IF above terrain && (previously grounded || previously ladder || previously airControl and not jumping || velocity is low)
-                else if ((tileWorld.GetFlags(Helpers.GetTile(belowRightFoot)).IsTerrain || tileWorld.GetFlags(Helpers.GetTile(belowLeftFoot)).IsTerrain)
-                    && (footing.Value == NavAgentFooting.Ground
-                        || footing.Value == NavAgentFooting.Ladder
-                        || (footing.Value == NavAgentFooting.AirControl && velocity.Linear.y <= (fix)0.5)
-                        || velocity.Linear.lengthSquared < 4))
+                else
                 {
-                    footing.Value = NavAgentFooting.Ground;
-                }
+                    OverlapPointInput detectTerrain = new OverlapPointInput()
+                    {
+                        Filter = SimulationGameConstants.Physics.TerrainFilter.Data,
+                    };
 
-                // GOTO air control IF in mid-air && was not in None
-                else if (footing.Value != NavAgentFooting.None)
-                {
-                    footing.Value = NavAgentFooting.AirControl;
+                    OverlapPointInput detectTerrainLeftFoot = detectTerrain;
+                    detectTerrainLeftFoot.Position = (float2)belowLeftFoot;
+                    OverlapPointInput detectTerrainRightFoot = detectTerrain;
+                    detectTerrainRightFoot.Position = (float2)belowRightFoot;
+
+                    // GOTO Ground IF above terrain && (previously grounded || previously ladder || previously airControl and not jumping || velocity is low)
+                    if ((physicsWorld.OverlapPoint(detectTerrainLeftFoot) || physicsWorld.OverlapPoint(detectTerrainRightFoot))
+                      && (footing.Value == NavAgentFooting.Ground
+                          || footing.Value == NavAgentFooting.Ladder
+                          || (footing.Value == NavAgentFooting.AirControl && velocity.Linear.y <= (fix)0.5)
+                          || velocity.Linear.lengthSquared < 4))
+                    {
+                        footing.Value = NavAgentFooting.Ground;
+                    }
+                    else
+                    {
+                        // GOTO air control IF in mid-air && was not in None
+                        if (footing.Value != NavAgentFooting.None)
+                        {
+                            footing.Value = NavAgentFooting.AirControl;
+                        }
+                    }
                 }
             }).Run();
 
