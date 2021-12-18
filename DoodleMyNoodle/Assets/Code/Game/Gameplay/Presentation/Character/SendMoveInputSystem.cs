09bba83fccf9ee30ac317deda001d80334081798
Frédéric Bessette <msfredb7@gmail.com>
2021-12-18 12:29:43 -0500

    jumping will now mute the vertical move inputs until the player releases the key. This prevents accidental ladder griping

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Character/SendMoveInputSystem.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Character/SendMoveInputSystem.cs
@@ -7,6 +7,7 @@ using UnityEngineX;
 
 public class SendMoveInputSystem : GamePresentationSystem<SendMoveInputSystem>
 {
+    private bool _muteVerticalInputUntilRelease;
     private DirtyValue<fix2> _moveInput;
 
     protected override void OnGamePresentationUpdate()
@@ -15,30 +16,40 @@ public class SendMoveInputSystem : GamePresentationSystem<SendMoveInputSystem>
             return;
 
         if (GameConsole.IsOpen())
-        {
             return;
-        }
+
+        // This should be done BEFORE directional move because Jumping can inhibit the 'up' move
+        HandleJump();
 
         HandleDirectionalMove();
-        HandleJump();
     }
 
     private void HandleDirectionalMove()
     {
         fix2 input = fix2.zero;
 
+        bool up = Input.GetKey(KeyCode.W);
+        bool down = Input.GetKey(KeyCode.S);
+        bool left = Input.GetKey(KeyCode.A);
+        bool right = Input.GetKey(KeyCode.D);
+        
+        if (_muteVerticalInputUntilRelease && !up && !down)
+        {
+            _muteVerticalInputUntilRelease = false;
+        }
+
         if (UIStateMachineController.Instance.CurrentSate.Type == UIStateType.Gameplay)
         {
-            if (Input.GetKey(KeyCode.D))
+            if (right)
                 input.x += 1;
 
-            if (Input.GetKey(KeyCode.A))
+            if (left)
                 input.x += -1;
 
-            if (Input.GetKey(KeyCode.W))
+            if (up && !_muteVerticalInputUntilRelease)
                 input.y += 1;
 
-            if (Input.GetKey(KeyCode.S))
+            if (down && !_muteVerticalInputUntilRelease)
                 input.y += -1;
         }
 
@@ -57,6 +68,10 @@ public class SendMoveInputSystem : GamePresentationSystem<SendMoveInputSystem>
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SimWorld.SubmitInput(new SimPlayerInputJump());
+
+            // This will mute all 'up' input until the player releases the 'up' key.
+            // This is used to prevent the player for accidentally grabbing onto ladders when pressing both W and jump together
+            _muteVerticalInputUntilRelease = true;
         }
     }
 }
