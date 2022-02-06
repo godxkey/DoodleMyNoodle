f1083af79c93b1cf160267e9294f856a88de99b2
Frédéric Bessette <msfredb7@gmail.com>
2022-02-06 13:00:29 -0500

    rework shield a bit

--- DoodleMyNoodle/Assets/Code/Simulation.Game/EntityComponents/Stats/Shield.cs
+++ DoodleMyNoodle/Assets/Code/Simulation.Game/EntityComponents/Stats/Shield.cs
@@ -1,16 +1,38 @@
 using Unity.Entities;
 
-[assembly: RegisterGenericComponentType(typeof(MaximumInt<Shield>))]
-[assembly: RegisterGenericComponentType(typeof(MinimumInt<Shield>))]
+[assembly: RegisterGenericComponentType(typeof(MaximumFix<Shield>))]
+[assembly: RegisterGenericComponentType(typeof(MaximumFix<Shield>))]
 
-public struct Shield : IComponentData, IStatInt
+public struct Shield : IComponentData, IStatFix
 {
-    public int Value;
-    public int RechargeRate;
-    public fix RechargeCooldown;
+    public fix Value;
 
-    int IStatInt.Value { get => Value; set => Value = value; }
+    fix IStatFix.Value { get => Value; set => Value = value; }
 
-    public static implicit operator int(Shield val) => val.Value;
-    public static implicit operator Shield(int val) => new Shield() { Value = val };
+    public static implicit operator fix(Shield val) => val.Value;
+    public static implicit operator Shield(fix val) => new Shield() { Value = val };
+}
+
+public struct ShieldRechargeRate : IComponentData
+{
+    public fix Value;
+
+    public static implicit operator fix(ShieldRechargeRate val) => val.Value;
+    public static implicit operator ShieldRechargeRate(fix val) => new ShieldRechargeRate() { Value = val };
+}
+
+public struct ShieldRechargeCooldown : IComponentData
+{
+    public fix Value;
+
+    public static implicit operator fix(ShieldRechargeCooldown val) => val.Value;
+    public static implicit operator ShieldRechargeCooldown(fix val) => new ShieldRechargeCooldown() { Value = val };
+}
+
+public struct ShieldLastHitTime : IComponentData
+{
+    public fix Value;
+
+    public static implicit operator fix(ShieldLastHitTime val) => val.Value;
+    public static implicit operator ShieldLastHitTime(fix val) => new ShieldLastHitTime() { Value = val };
 }
