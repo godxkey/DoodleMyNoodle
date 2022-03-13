0f2b239c5898bf655dc09c959eb987a2346f855e
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2022-03-13 14:22:47 -0400

    Nouveau visual de Mobs

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Animations/SpriteChangeAnimationDefinition.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Presentation/Animations/SpriteChangeAnimationDefinition.cs
@@ -1,4 +1,5 @@
 ﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class SpriteChangeAnimationDefinition : AnimationDefinition
     [FormerlySerializedAs("NewSprite")]
     [SerializeField] private Sprite _newSprite;
     [SerializeField] private float _duration = 0.5f;
+    [SerializeField] private bool _loop = false;
 
     private Dictionary<int, Sprite> _initialSprites = new Dictionary<int, Sprite>();
 
@@ -19,14 +21,33 @@ public class SpriteChangeAnimationDefinition : AnimationDefinition
         // remember the old sprite
         _initialSprites[input.TriggerId] = input.PresentationTarget.SpriteRenderer.sprite;
 
-        // set new sprite
+        ouput.Duration = _loop ? -1 : _duration;
+
         input.PresentationTarget.SpriteRenderer.sprite = _newSprite;
 
-        ouput.Duration = _duration;
+        if (_loop)
+        {
+            input.PresentationTarget.Root.GetComponent<MonoBehaviour>().StartCoroutine(SpriteLoop(input));
+        }
+    }
+
+    IEnumerator SpriteLoop(TriggerInput input)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_duration);
+
+            input.PresentationTarget.SpriteRenderer.sprite = _initialSprites[input.TriggerId];
+
+            yield return new WaitForSeconds(_duration);
+
+            input.PresentationTarget.SpriteRenderer.sprite = _newSprite;
+        }
     }
 
     public override void StopAnimation(StopInput input)
     {
+        input.PresentationTarget.Root.GetComponent<MonoBehaviour>().StopAllCoroutines();
         input.PresentationTarget.SpriteRenderer.sprite = _initialSprites[input.TriggerId];
         _initialSprites.Remove(input.TriggerId);
     }
