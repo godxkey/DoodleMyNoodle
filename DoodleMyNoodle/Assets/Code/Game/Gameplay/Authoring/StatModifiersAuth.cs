e2eba2561f1a172cf8f5007bbc4433db8a41f0f8
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2022-05-21 15:10:35 -0400

    Archetype Modifiers + Proto7 Level First pass

--- DoodleMyNoodle/Assets/Code/Game/Gameplay/Authoring/StatModifiersAuth.cs
+++ DoodleMyNoodle/Assets/Code/Game/Gameplay/Authoring/StatModifiersAuth.cs
@@ -21,4 +21,19 @@ public class StatModifiersAuth : MonoBehaviour, IConvertGameObjectToEntity
             entities.Add(new StartingStatModifier() { Type = statusEffect });
         }
     }
+
+    void OnDrawGizmos()
+    {
+        if (StartingStatModifiers.Contains(StatModifierType.Armored))
+        {
+            Gizmos.color = Color.gray;
+            Gizmos.DrawSphere(transform.position + new Vector3(-0.2f, 0, 0), 0.1f);
+        }
+
+        if (StartingStatModifiers.Contains(StatModifierType.Brutal))
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position + new Vector3(0.2f, 0,0), 0.1f);
+        }
+    }
 }
