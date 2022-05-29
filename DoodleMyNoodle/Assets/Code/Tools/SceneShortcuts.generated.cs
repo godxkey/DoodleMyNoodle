b211a2a8afd6fa916170d6bedde916217f8b278f
Alexandre Blanchet <alexandre.blanchet.94@gmail.com>
2022-05-29 12:20:11 -0400

    Empty Level + New Proto 7 waves

--- DoodleMyNoodle/Assets/Code/Tools/SceneShortcuts.generated.cs
+++ DoodleMyNoodle/Assets/Code/Tools/SceneShortcuts.generated.cs
@@ -43,28 +43,31 @@ public static class SceneShortcuts
     [MenuItem("Scene Shortcuts/Gym_Surveys", priority = 4011)]
     public static void Assets_Scenes_Levels_Scene_Assets_Gym_Surveys() => LoadScene("Assets/Scenes/Levels/Scene Assets/Gym_Surveys.unity");
 
-    [MenuItem("Scene Shortcuts/Lvl_Prototype7", priority = 4012)]
+    [MenuItem("Scene Shortcuts/Lvl_EmptyLevel", priority = 4012)]
+    public static void Assets_Scenes_Levels_Scene_Assets_Lvl_EmptyLevel() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_EmptyLevel.unity");
+
+    [MenuItem("Scene Shortcuts/Lvl_EmptyLevel_Presentation", priority = 4013)]
+    public static void Assets_Scenes_Levels_Scene_Assets_Lvl_EmptyLevel_Presentation() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_EmptyLevel_Presentation.unity");
+
+    [MenuItem("Scene Shortcuts/Lvl_Prototype7", priority = 4014)]
     public static void Assets_Scenes_Levels_Scene_Assets_Lvl_Prototype7() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_Prototype7.unity");
 
-    [MenuItem("Scene Shortcuts/Lvl_Prototype7_Presentation", priority = 4013)]
+    [MenuItem("Scene Shortcuts/Lvl_Prototype7_Presentation", priority = 4015)]
     public static void Assets_Scenes_Levels_Scene_Assets_Lvl_Prototype7_Presentation() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_Prototype7_Presentation.unity");
 
-    [MenuItem("Scene Shortcuts/Lvl_SideScrollerExemple", priority = 4014)]
-    public static void Assets_Scenes_Levels_Scene_Assets_Lvl_SideScrollerExemple() => LoadScene("Assets/Scenes/Levels/Scene Assets/Lvl_SideScrollerExemple.unity");
-
-    [MenuItem("Scene Shortcuts/Menu_InGameEscape", priority = 5015)]
+    [MenuItem("Scene Shortcuts/Menu_InGameEscape", priority = 5016)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_InGameEscape() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_InGameEscape.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineClientSessionChoice", priority = 5016)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineClientSessionChoice", priority = 5017)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineClientSessionChoice() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineClientSessionChoice.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineRoleChoice", priority = 5017)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineRoleChoice", priority = 5018)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineRoleChoice() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineRoleChoice.unity");
 
-    [MenuItem("Scene Shortcuts/Menu_OnlineServerSessionCreation", priority = 5018)]
+    [MenuItem("Scene Shortcuts/Menu_OnlineServerSessionCreation", priority = 5019)]
     public static void Assets_Scenes_Menu_Scene_Assets_Menu_OnlineServerSessionCreation() => LoadScene("Assets/Scenes/Menu/Scene Assets/Menu_OnlineServerSessionCreation.unity");
 
-    [MenuItem("Scene Shortcuts/Misc_LoadingScreen", priority = 6019)]
+    [MenuItem("Scene Shortcuts/Misc_LoadingScreen", priority = 6020)]
     public static void Assets_Scenes_Misc_Scene_Assets_Misc_LoadingScreen() => LoadScene("Assets/Scenes/Misc/Scene Assets/Misc_LoadingScreen.unity");
 
 
